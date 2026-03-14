6b9f556c0ada9bc73cdd918552a05e8fff85816d
MiguelAngelOrtin <1319295@alu.murciaeduca.es>
2026-03-14 14:20:52 +0100

    home - hero y seccion juegos destacados base

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,33 @@
 @section('title', 'Inicio')
 
 @section('content')
-<section class="hero">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-            <h1 class="text-center">Discover your next great indie</h1>
-            <p class="text-center">The platform where indie developers share their games and players discover them before anyone else.</p>
-            <div class="text-center">
-                <a href="/games" class="btn-register">Games</a>
+    <section class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center">Discover your next great indie</h1>
+                    <p class="text-center">The platform where indie developers share their games and players discover them
+                        before anyone else.</p>
+                    <div class="text-center">
+                        <a href="/games" class="btn-register">Games</a>
+                    </div>
                 </div>
+            </div>
         </div>
+    </section>
+
+    {{-- Cards --}}
+    <section>
+        <div class="container">
+            <div class="row">
+            <div class="col-4">
+            <ul class="list-group">
+                <li class="list-group-item">Hollow Knight - Metroidvania</li>
+                <li class="list-group-item">Hades - Roguelike</li>
+            </ul>
+                
         </div>
     </div>
-    </section>
+        </div>
+    </section>  
 @endsection
