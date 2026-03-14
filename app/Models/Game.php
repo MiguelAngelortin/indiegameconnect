8a9ec1757889dcac5813b7fe65ea36e6031e3a3f
MiguelAngelOrtin <1319295@alu.murciaeduca.es>
2026-03-14 12:29:35 +0100

    modelos Game y Genre con relaciones

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -21,4 +21,8 @@ class Game extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function genres(){
+        return $this->belongsToMany(Genre::class);
+    }
 }
