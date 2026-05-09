3885fdb40917ebad86749a375a7b51eedd92a7e2
MiguelAngelOrtin <1319295@alu.murciaeduca.es>
2026-05-09 21:29:22 +0200

    fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,28 +2,18 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use App\Models\Genre;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\AdminSeeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        $this->call([GenreSeeder::class,
-        AdminSeeder::class,
-        ]);
-        $this->call(IndieGamesSeeder::class);
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            GenreSeeder::class,
+            IndieGamesSeeder::class,
         ]);
     }
 }
